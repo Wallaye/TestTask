4cabb11524df29a9b76dffda001c96e7746bf5b2
Wallaye <pavel.vashkevich03@gmail.com>
2023-09-28 15:39:22 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class OrderService : IOrderService
+    {
+        private ApplicationDbContext _context;
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public Task<Order> GetOrder()
+        {
+            var order = _context.Orders
+                .OrderByDescending(o => o.Price * o.Quantity)
+                .FirstOrDefaultAsync();
+            return order;
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            var orders = _context.Orders
+                .Where(o => o.Quantity > 10)
+                .ToListAsync();
+            return orders;
+        }
+    }
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class UserService : IUserService
+    {
+        private ApplicationDbContext _context;
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<User> GetUser()
+        {
+            var user = _context.Users
+                .Include(u => u.Orders)
+                .OrderByDescending(u => u.Orders.Count())
+                .FirstOrDefaultAsync();
+            return user;
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            var inactiveUsers = _context.Users
+                .Where(u => u.Status == UserStatus.Inactive)
+                .ToListAsync();
+            return inactiveUsers;
+        }
+    }
+}
